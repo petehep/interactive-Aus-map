280088cf25a2827e8065a9194bca37b1e18e6ff1
petehep <petehep@gmail.com>
2025-11-24 19:11:22 +1030

    SECURITY: Move Firebase keys to environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWdcEBNoobKZoNRQmWlooCJNCBfb_M8-U",
-  authDomain: "lapmap-cc281.firebaseapp.com",
-  projectId: "lapmap-cc281",
-  storageBucket: "lapmap-cc281.firebasestorage.app",
-  messagingSenderId: "102969245328",
-  appId: "1:102969245328:web:1b3478a17782fc6f792215",
-  measurementId: "G-4R5VTXQ9X4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
